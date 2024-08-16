d293e9944d66ecd29bf882fd8905eff16784e1e6
Mohamed Elsayed <86891363+Mohamedzonkol@users.noreply.github.com>
2024-08-17 01:50:48 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,13 +10,7 @@ variable "vm_size" {
   default = "Standard_B1ms"
 }
 
-variable "admin_username" {
-  default = "adminuser"
-}
 
-variable "admin_password" {
-  default = "P@ssword1234!"
-}
 
 variable "dns_prefix" {
   default = "testnode"
